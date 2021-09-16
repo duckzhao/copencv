a54d3c1a768808fed11404694d8fd0e1ca420daf
1161678627 <1161678627@qq.com>
2021-09-17 00:21:27 +0800

    测试opencv是否安装成功

--- copencv/test.cpp
+++ copencv/test.cpp
@@ -1,5 +1,15 @@
 #include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace cv;
 
 int main() {
+	string path = "Resources/test.png";
+	Mat img = imread(path);
+	imshow("image", img);
+	waitKey(0);
 	return 0;
 }
